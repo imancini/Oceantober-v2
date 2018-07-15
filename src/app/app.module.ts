e688c4ab8843934dd8d266cdf9f8c09d76b2f65c
Isaac <imancini@hq.bill.com>
2018-07-15 12:06:15 -0700

    Oceantober - first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { OurWorkComponent } from './our-work/our-work.component';
+import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'ourWork', component: OurWorkComponent },
+  // { path: 'references', component: ReferencesComponent },
+  // { path: 'about', component: AboutComponent },
+  // { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OurWorkComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    ScrollToModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
